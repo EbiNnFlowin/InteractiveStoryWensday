4e269f606653a8a7a216b734a4a268452e8d2775
ebintobin <55466612+ebintobin@users.noreply.github.com>
2020-05-06 17:57:49 -0400

    end of day 3

--- app/src/main/java/com/example/interactivestorywensday/MainActivity.java
+++ app/src/main/java/com/example/interactivestorywensday/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.interactivestorywensday;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -35,7 +36,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void startStory(String name) {
         Intent intent = new Intent(this,StoryActivity.class);
-        intent.putExtra("name",name);
+        Resources resources = getResources();
+        String key = resources.getString(R.string.key_name);
+        intent.putExtra(key,name);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/interactivestorywensday/StoryActivity.java
+++ app/src/main/java/com/example/interactivestorywensday/StoryActivity.java
@@ -4,16 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class StoryActivity extends AppCompatActivity {
-
+    private static final String TAG = StoryActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
         Intent intent = getIntent();
-        String name = intent.getStringExtra("name");
-        Toast.makeText(this, name, Toast.LENGTH_LONG).show();
+        String name = intent.getStringExtra(getString(R.string.key_name));
+        if (name == null || name.isEmpty()){
+            name = "Player";
+        }
+
+        Log.d(TAG, name);
     }
 }
